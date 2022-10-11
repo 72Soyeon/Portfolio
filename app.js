b4403eb5a091c99f581d3bffb01571e89345a441
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-10-11 17:47:23 -0400

    First Commit
    Portfolio

---
+++ app.js
@@ -0,0 +1,19 @@
+// let listItems=[...document.querySelectorAll('title')];
+
+// let options = {
+//     rootMargin: '-10%',
+//     threshold: 0.0
+// }
+
+// let observer= new IntersectionObserver(showItem, options);
+
+// function showItem(entries){
+//     //console.log(entries);
+//     entries.forEach(entry=>{
+//         entry.target.classList.add('name');
+//     })
+// }
+
+// listItems.forEach(item=>{
+//     observer.observe(item);
+// })
