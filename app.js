1efcec5523669a36f4e1ac55eda78837e4b8a848
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-10-11 21:16:16 -0400

    101122 Responsive
    Responsive Adjustment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener("pageshow", function(){
         paused:true
 	});
 
-	tl_intro.to("html", 0.8, {
+	tl_intro.to("html", 0.6, {
 		autoAlpha: 1,
 		ease: Power1.easeOut
 	});
@@ -62,3 +62,52 @@ tl_transition01
     });
     
 tl_transition01.timeScale(0.5);
+
+
+
+[...document.querySelectorAll('.stagger_body_trigger')].forEach(function(item) {
+	var staggers = item.querySelectorAll('.stagger_body');
+	var tl02 = gsap.timeline({
+		paused: true
+	});
+	tl02.staggerFromTo(staggers, 1.5 	, {
+		y: '100%',
+		skewY: 10,
+		autoAlpha: 0
+	}, {
+		y: '0%',
+		skewY: 0,
+		autoAlpha: 1,
+		ease: Power4.easeOut
+	}, 0.3);
+	ScrollTrigger.create({
+		trigger: item,
+		start: "center bottom",
+		onEnter: () => tl02.play()
+	});
+});
+
+
+[...document.querySelectorAll('.more_stagger_body_trigger')].forEach(function(item) {
+	var staggers = item.querySelectorAll('.more_stagger_body');
+	var tl02 = gsap.timeline({
+		paused: true
+	});
+
+	tl02.staggerFromTo(staggers, 2, {
+		y: '50%',
+		skewY: 2,
+		autoAlpha: 0
+	}, {
+		y: '0%',
+		skewY: 0,
+		autoAlpha: 1,
+		ease: Power4.easeOut
+	}, 0.3);
+
+	ScrollTrigger.create({
+		trigger: item,
+		start: "left bottom",
+		onEnter: () => tl02.play()
+	});
+});
